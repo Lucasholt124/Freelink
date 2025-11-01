7a9f60941043bcd3aca054a31bfb5eafe345757b
Lucasholt124 <lucasholt2021@gmail.com>
2025-11-01 13:39:53 -0300

    corrrigindo erro

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -4,6 +4,7 @@ import { clerkClient } from "@clerk/nextjs/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2025-07-30.basil',
+
 });
 
 export async function POST(req: Request) {
