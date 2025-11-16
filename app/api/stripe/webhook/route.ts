4adf6a3498ff2fae7c30118b4c43a3f96b08757f
Lucasholt124 <lucasholt2021@gmail.com>
2025-11-16 14:09:26 -0300

    redeploy

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -4,6 +4,7 @@ import { clerkClient } from "@clerk/nextjs/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2025-08-27.basil',
+
 });
 
 export async function POST(req: Request) {
